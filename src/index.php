71c7a879c64c6d8a0c81aec1767854cdcca44644
Kevin Washington <kevin.washingtonbr@gmail.com>
2018-12-08 18:58:32 -0300

    Initial content

---
+++ src/index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <title>Sistema de Gerenciamento de Atividade Acadêmica</title>
+</head>
+
+<body>
+  <h1>Hello World!</h1>
+  <script src="assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
