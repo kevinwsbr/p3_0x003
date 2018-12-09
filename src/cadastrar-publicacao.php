72d2faf735f2ad05533f48891698be5ef60173c4
Kevin Washington <kevin.washingtonbr@gmail.com>
2018-12-09 14:44:48 -0300

    Publication object

---
+++ src/cadastrar-publicacao.php
@@ -0,0 +1,94 @@
+<?php
+
+require_once 'configs/Database.php';
+require_once 'configs/Collaborator.php';
+require_once 'configs/Project.php';
+require_once 'configs/Publication.php';
+
+$conn = new Database();
+$collaborator = new Collaborator($conn->db);
+$project = new Project($conn->db);
+$publication = new Publication($conn->db);
+
+$collaborators = $collaborator->getCollaborators();
+$teachers = $collaborator->getTeachers();
+$projects = $project->getInProgressProjects();
+$publication->register();
+?>
+
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <title>Cadastrar Publicação | SGPA</title>
+</head>
+
+<body>
+  <?php require_once "components/header.php"; ?>
+  <section class="container">
+    <div class="row my-3">
+      <div class="col col-12">
+        <h3>Cadastrar nova publicação</h3>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col col-12">
+        <form action="cadastrar-publicacao.php" method="POST">
+            <div class="row">
+                <div class="col">
+                    <div class="form-group">
+                    <label for="title">Título</label>
+                    <input required type="text" name="title" class="form-control" id="title">
+                </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                    <label for="conference_name">Conferência de publicação</label>
+                    <input required type="text" name="conference_name" class="form-control" id="conference_name">
+                </div>
+                </div>
+                <div class="col col-2">
+                    <div class="form-group">
+                    <label for="year">Ano de publicação</label>
+                    <input required type="text" name="year" class="form-control" id="year">
+                </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="form-group">
+                        <label for="project">Projeto associado</label>
+                        <select required name="project" class="form-control" id="project">
+                        <option selected value="0">n/a</option>
+                        <?php foreach($projects as $pro) { ?>
+                        <option value="<?=$pro['ID']?>"><?=$pro['title']?></option>
+                        <?php } ?>
+                        </select>
+                    </div>
+                </div>
+                <div class="col">
+                    <h5>Autores</h5>
+            <?php foreach ($collaborators as $col) { ?>
+                <div class="form-check-inline">
+                    <input class="form-check-input" name="members[]" type="checkbox" value="<?=$col['ID']?>" id="check_<?=$col['ID']?>">
+                    <label class="form-check-label" for="check_<?=$col['ID']?>">
+                            <?php echo $col['name']; ?>
+                        </label>
+                    </div>
+                    <?php } ?>
+                </div>
+            </div>
+          <button type="submit" class="btn btn-success mb-4">Cadastrar</button>
+        </form>
+      </div>
+    </div>
+  </section>
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/popper.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ src/configs/Publication.php
@@ -0,0 +1,137 @@
+<?php
+
+class Publication {
+    protected $ID;
+    protected $title;
+    protected $conferenceName;
+    protected $year;
+    protected $authors;
+    protected $db;
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    public function getID ()
+    {
+        return $this->ID;
+    }
+
+    public function getTitle ()
+    {
+        return $this->title;
+    }
+
+    public function getConferenceName ()
+    {
+        return $this->conferenceName;
+    }
+
+    public function getYear ()
+    {
+        return $this->year;
+    }
+
+    public function getAuthors ()
+    {
+        return $this->authors;
+    }
+
+    public function setData($publication) {
+        $this->ID = $publication['ID'];
+        $this->title = $publication['title'];
+        $this->conferenceName = $publication['conferenceName'];
+        $this->year = $publication['year'];
+        $this->authors = $publication['authors'];
+    }
+    
+    public function getPublications() {
+        $sql = 'SELECT * from `publications`;';
+
+        $db=$this->db->prepare($sql);
+        $db->execute();
+
+        return $db->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /*public function getProject() {
+        if ($_SERVER['REQUEST_METHOD']=='GET') {
+            $sql='SELECT * FROM `projects` WHERE `ID` = :ID ;';
+
+            $db=$this->db->prepare($sql);
+            $db->bindValue(':ID', $_GET['id'], PDO::PARAM_STR);
+            $db->execute();
+
+            $this->setData($db->fetch(PDO::FETCH_ASSOC));
+        }
+    }
+
+    public function getInPreparationProjects() {
+        $sql = 'SELECT * from `projects` WHERE `status` = "in_preparation";';
+
+        $db=$this->db->prepare($sql);
+        $db->execute();
+
+        return $db->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getInProgressProjects() {
+        $sql = 'SELECT * from `projects` WHERE `status` = "in_progress";';
+
+        $db=$this->db->prepare($sql);
+        $db->execute();
+
+        return $db->fetchAll(PDO::FETCH_ASSOC);
+    }*/
+
+    public function insertAuthors ($idPublication, $idAuthor) {
+         $sql='INSERT INTO `publications_and_collaborators` (`id_publication`, `id_collaborator`) VALUES (:idPublication, :idCollaborator);';
+            
+        $db=$this->db->prepare($sql);
+        $db->bindValue(':idPublication', $idPublication, PDO::PARAM_STR);
+        $db->bindValue(':idCollaborator', $idAuthor, PDO::PARAM_STR);
+            
+        $db->execute();
+    }
+
+    public function register() {
+        if ($_SERVER['REQUEST_METHOD']=='POST') {
+            print_r($_POST);
+            $sql='INSERT INTO `publications` (`title`, `conference_name`, `year`, `id_project`) VALUES (:title, :conferenceName, :year, :idProject);';
+            
+            if(empty($_POST['project']) || $_POST['project'] == '0') {
+                $project = NULL;
+            } else {
+                $project = $_POST['project'];
+            }
+
+            $db=$this->db->prepare($sql);
+            $db->bindValue(':title', $_POST['title'], PDO::PARAM_STR);
+            $db->bindValue(':conferenceName', $_POST['conference_name'], PDO::PARAM_STR);
+            $db->bindValue(':year', $_POST['year'], PDO::PARAM_STR);
+            $db->bindValue(':idProject', $project, PDO::PARAM_STR);
+            
+            $db->execute();
+
+            $sql='SELECT ID FROM `publications` WHERE `title` = :pubTitle;';
+
+            $db=$this->db->prepare($sql);
+            $db->bindValue(':pubTitle', $_POST['title'], PDO::PARAM_STR);
+
+            $db->execute();
+
+            $pub = $db->fetch(PDO::FETCH_ASSOC);
+
+            if(isset($_POST['members']) && !empty($_POST['members'])) {
+                foreach($_POST['members'] as $member) {
+                    $this->insertAuthors($pub['ID'], $member);
+                }
+            }
+            
+            header('Location: index.php');
+        }
+    }
+    
+}
+?>
